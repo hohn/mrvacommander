467b0a37361493d2c095994115d5423623070901
Michael Hohn <mhhohn@gmail.com>
2024-05-08 23:11:41 -0700

    wip

---
+++ cmd/commander/doc.go
@@ -0,0 +1,4 @@
+// Copyright © 2024 github
+// Licensed under the Apache License, Version 2.0 (the "License").
+
+package main
---
+++ cmd/commander/main.go
@@ -0,0 +1,7 @@
+// Copyright © 2024 github
+// Licensed under the Apache License, Version 2.0 (the "License").
+package main
+
+func main() {
+	Execute()
+}
